cbebd0733f8f1d7a6e090cffa30abf39f69d6342
elidvenega <venegaelid@gmail.com>
2022-09-23 16:32:50 -0700

    first commit

---
+++ src/components/first.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const data = {
+  name: 'James',
+  hobby: 'guitar'
+}
+
+function Test() {
+  return (
+    <>
+    <h1>Hellooo it works</h1>
+    </>
+  )
+}
+
+export const Buttons = () => {
+  return (
+    <div className='component_one'>
+      <h1 style={{
+        backgroundColor: "green",
+        color: "blue"
+      }}>{data.name}</h1>
+      <Test />
+    </div>
+  )
+}
