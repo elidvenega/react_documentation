c04d45dd8cfa9393b47478a49644c17e5e51dfcc
elidvenega <venegaelid@gmail.com>
2022-10-10 21:46:55 -0700

    Updated

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 // import  CurlyBraces  from "./components/jsxcurlybraces.component";
 // import Props  from "./components/props.component";
 // import PackingList from "./components/conditional.component";
-import Interactivity from './components/interactivity/interactivity.component';
+// import Interactivity from './components/interactivity/interactivity.component';
+import Ref from "./components/escape_hatches/ref.component";
 
-import './app.css';
+import "./app.css";
 
 function App() {
   return (
     <div className="App">
-   {/* <CurlyBraces /> */}
-   {/* <Props  name="eli"/>
+      {/* <CurlyBraces /> */}
+      {/* <Props  name="eli"/>
    <Props  name="Jake"/>
    <Props  name="David"/> */}
-   {/* <PackingList /> */}
-   <Interactivity />
+      {/* <PackingList /> */}
+      {/* <Interactivity /> */}
+      <Ref />
     </div>
   );
 }
